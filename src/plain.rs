d7f3b6ed96ca61d5b5bc09b74f18b1cc451c7e17
Wim Looman <wim@nemo157.com>
2020-03-31 12:58:15 +0200

    Support io::Write for plain

--- src/plain.rs
+++ src/plain.rs
@@ -5,6 +5,11 @@ pub struct String {
     inner: std::string::String,
 }
 
+#[derive(Clone, Debug, Default)]
+pub struct Write<T> {
+    inner: T,
+}
+
 impl String {
     pub fn new() -> Self {
         Self::default()
@@ -15,11 +20,30 @@ impl String {
     }
 }
 
+impl<T> Write<T> {
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
+}
+
 impl stylish::Write for String {
     type Error = std::fmt::Error;
 
     fn write_str(&mut self, s: &str, _style: Style) -> std::fmt::Result {
         self.inner.push_str(s);
+        Ok(())
+    }
+}
+
+impl<T: std::io::Write> stylish::Write for Write<T> {
+    type Error = std::io::Error;
+
+    fn write_str(&mut self, s: &str, _style: Style) -> std::io::Result<()> {
+        self.inner.write_all(s.as_bytes())?;
         Ok(())
     }
 }
