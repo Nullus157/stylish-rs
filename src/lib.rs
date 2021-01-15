a3094872ad9d9a59be6bb73e163ba3120550a116
Wim Looman <git@nemo157.com>
2021-01-15 21:10:22 +0100

    Add helper methods for wrapping std writers

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,18 @@ pub use stylish_core::{
     format_args, io, write, writeln, Arguments, Color, Display, Formatter, Intensity, Style, Write,
 };
 
+pub fn ansi<T: std::fmt::Write>(inner: T) -> ansi::Ansi<T> {
+    ansi::Ansi::new(inner)
+}
+
+pub fn html<T: std::fmt::Write>(inner: T) -> html::Html<T> {
+    html::Html::new(inner)
+}
+
+pub fn plain<T: std::fmt::Write>(inner: T) -> plain::Plain<T> {
+    plain::Plain::new(inner)
+}
+
 #[doc(inline)]
 pub use stylish_ansi as ansi;
 #[doc(inline)]
