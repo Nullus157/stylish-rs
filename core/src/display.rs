e6a4df3bdedf1533c1259edd4215a1dc54b44a34
Wim Looman <git@nemo157.com>
2021-04-27 22:05:47 +0200

    Start docs on Display trait

--- core/src/display.rs
+++ core/src/display.rs
@@ -1,5 +1,9 @@
 use crate::{Formatter, Result};
 
+/// Format trait for the `stylish` format, `{:s}`.
+///
+/// `Display` is similar to [`core::fmt::Display`], but allows attaching additional style
+/// attributes to the output.
 pub trait Display {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result;
 }
